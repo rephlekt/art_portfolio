8a9a6194e05276afcb623ee01b8291b49a401710
rephlekt <rephlekt2718@gmail.com>
2021-09-04 16:39:54 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const path = require("path")
+const app = express ()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "node_modules")))
+
+app.get("/", (req, res) => {
+    res.sendFile("public/views/index.html", {root: __dirname})
+})
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`)
+})
