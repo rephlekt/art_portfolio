c7362ec8df1f775fc0dc7abe3cd091b533b234c6
rephlekt <rephlekt2718@gmail.com>
2021-09-11 21:26:44 -0500

    sending json now through host/images path

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
 const express = require("express")
 const path = require("path")
+const ejs = require("ejs")
 const util = require("util")
-const  { exec }  = require('child_process');
+const exec = util.promisify(require('child_process').exec);
 
 const app = express ()
 const port = 3000
 
-exec("ls -l " + path.join(__dirname, "public/images/") + " | cut -d' ' -f 10", (err, stdout, stderr) => {
-    console.log(stdout)
-    //make json creating in here
-})
+async function getImageJSON(){
+    items = []
+    try{
+        const { stdout, stderr } = await exec("ls -l " + path.join(__dirname, "public/images/") + " | cut -d' ' -f 10")
+        let lines = stdout.split("\n").filter( item=>item )
 
+        for (let i = 0; i < lines.length; i++){
+
+            const{ stdout, stderr } = await exec("identify -format '%w %h' public/images/" + lines[i])
+
+            let image_info = {
+                src: "images/" + lines[i],
+                w: stdout.split(" ")[0],
+                h: stdout.split(" ")[1],
+                title: lines[i].split(".")[0].replaceAll("_", " ")
+            }
+            items.push(image_info)   
+        }
+        return items
+    }
+    catch(e){
+        console.error(e)
+    }
+}
+
+app.set("views", path.join(__dirname, "public/views/"))
+app.set("view engine", "ejs")
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/js", express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "public/images")))
 app.use(express.static(path.join(__dirname, "node_modules")))
 
 app.get("/", (req, res) => {
-    res.sendFile("public/views/index.html", {root: __dirname})
+    res.render("index.ejs", {root: __dirname})
+})
+
+app.get("/images", async (req, res) => {
+    items = await getImageJSON()
+    res.json( items[0] )
 })
 
 app.listen(port, () => {
